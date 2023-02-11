d0a3fadfc28ab286e16cc286ee21e87ef56297b1
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-02-10 23:22:53 -0500

    Added changes for GitHub.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <header className="App-header">
+        <p className="page-title">
+          The <br />
+          Hard <br />
+          Problem
+        </p>
+        
+      </header>
+    </section>
+  );
+}
+
+export default App;
