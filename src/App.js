0c0741beb07005179b7da2755f7ea0a9c444fa3a
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-02-11 01:50:24 -0500

    Added brain clip-path.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <section className="App">
-      <header className="App-header">
-        <p className="page-title">
-          The <br />
-          Hard <br />
-          Problem
-        </p>
-        
-      </header>
-    </section>
-  );
+	return (
+		<section className="App">
+		{/*<header className="App-header">
+			<p className="page-title">
+			The <br />
+			Hard <br />
+			Problem
+			</p>        
+		</header>*/}
+		<section className="brain" >
+			<p className="title-text">
+				The 
+				Hard <br />
+				Problem <br />
+			</p>
+			
+		</section>
+
+		</section>
+	);
 }
 
 export default App;
