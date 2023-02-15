b0e04d80b3bb68cc6eca6f6fe9f0b883ff7cb039
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-02-14 23:18:39 -0500

    Edits to CSS animations.

--- src/App.js
+++ src/App.js
@@ -26,10 +26,30 @@ function App() {
 			
 		</section>
 
+
+
 		<section className="body">
+		<section class="box">
+			<img class="clipped-image" src="https://homepages.cae.wisc.edu/~ece533/images/arctichare.png" />
+			<p class="clipped-text">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			</p>
+		</section>
+
+			<section className="brain-image test-width"></section>
+			<section className="apple test-width"></section>
+			<section className="bat test-width"></section>
 			<section className="text-box" >
 				The hard problem of consciousness is one of the hardest problems in philosophy today. In simple terms, it asks "why is it like something to have the experiences we have?"
 			</section>
+
+			
+			<div className="brain-image"></div>
+			<div className="apple"></div>
+			<div className="bat"></div>
+			
+
+			<div className="hexagone"></div>
 
 			<div className="hexagone"></div>
 			<div className="triangle"></div>
