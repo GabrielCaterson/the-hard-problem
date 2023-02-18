1b399fd6d2d5eff807ceccaf8634f9ea04d1fa73
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-02-17 20:53:17 -0500

    Added mug SVG.

--- src/App.js
+++ src/App.js
@@ -4,94 +4,79 @@ import './App.css';
 function App() {
 	return (
 		<section className="App">
-		{/*<header className="App-header">
-			<p className="page-title">
-			The <br />
-			Hard <br />
-			Problem
-			</p>        
-		</header>*/}
+			{/*<header className="App-header">
+				<p className="page-title">
+				The <br />
+				Hard <br />
+				Problem
+				</p>        
+			</header>*/}
 
 
-		{/*<section className="logo-container">
-			<section className="logo"> 
-				<p className="title-text">
-					The 
-					Hard <br />
-					Problem <br />
-				</p>
-			</section>
-		</section>*/}
+			{/*<section className="logo-container">
+				<section className="logo"> 
+					<p className="title-text">
+						The 
+						Hard <br />
+						Problem <br />
+					</p>
+				</section>
+			</section>*/}
 
-		<section className="logo-container">
-			<section  className="logo-container-inner">
-				<section className="animated-logo"> 
+			<section className="logo-container">
+				<section  className="logo-container-inner">
+					<section className="animated-logo"> 
+					
+					</section>
+		
+					<p className="title-text">
+						The 
+						Hard <br />
+						Problem <br />
+					</p>
+				</section>
 				
-				</section>
-	
-				<p className="title-text">
-					The 
-					Hard <br />
-					Problem <br />
-				</p>
-			</section>
-			
-		</section>
-
-
-
-		<section className="body">
-
-			<section className="test-sword-container">
-				<section className="test-sword">
-				test test test test test test test test test test test test test test test test test test test test test test test test test test test 
-				</section>
 			</section>
 
-			<section class="box">
-				<img class="clipped-image" src="https://homepages.cae.wisc.edu/~ece533/images/arctichare.png" />
-				<p class="clipped-text">
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-				</p>
-			</section>
 
-				<section className="brain-image test-width"></section>
-				<section className="apple test-width"></section>
-				<section className="bat test-width"></section>
-				<section className="text-box" >
-					The hard problem of consciousness is one of the hardest problems in philosophy today. In simple terms, it asks "why is it like something to have the experiences we have?"
+
+			<section className="body">
+
+				<section className="text-box-container">
+					<section className="text-box" >
+						The hard problem of consciousness is one of the hardest problems in philosophy today. In simple terms, it asks "why is it like something to have the experiences we have?" Why is it like something to see the color red, smell coffee, or taste strawberries? 
+					</section>
+
+					<section className="mug"> 
+
+					</section>
 				</section>
 
-				
-				<div className="brain-image"></div>
-				<div className="apple"></div>
-				<div className="bat"></div>
-				
 
-				<div className="hexagone"></div>
+				<section className="text-box-container">
+					<section className="text-box" >
+						Lorem ipsum dolor sit amet, simul oratio elaboraret eum ea, iudicabit scripserit id sit, summo discere accumsan ei sit. Sed temporibus interpretaris in, elit ferri verterem vis et. Alii quaeque quaestio an nam, mei ne habeo rationibus definitiones, vix ludus senserit persecuti ad. Graece nominavi te est, ut dicta delicata his, invidunt detraxit ex vis. Ut modo clita iuvaret vix. Ea qui alii purto ancillae.
 
-				<div className="hexagone"></div>
-				<div className="triangle"></div>
-				<div className="star" ></div>
-				<div className="arrow" ></div>
-				<div className="custom"></div>
-				<div className="brain-image"></div>
-				<div className="test"> blah </div>
+						Pri possit nostrum id, aliquam tincidunt intellegat eu duo. Mea errem deterruisset ad, pro in duis accusata contentiones, ei vim liber paulo utroque. No quo laudem animal pertinax, eam ad hinc assentior theophrastus, eum ea error epicuri philosophia. Tollit apeirian ad duo, feugait verterem cum te. Est eirmod oportere persequeris at, vidit etiam nec eu, ius cu graeci prompta habemus.
+
+						Sit affert quaestio ne, ei pro debet pertinax percipitur. Ignota invenire constituto et mea. Audiam malorum sit eu. Pro ad laudem inermis, nam ut quem aperiri alienum, nam et omnis movet animal. Cu quo error philosophia, et impedit tractatos per.
+
+						At eam feugait torquatos, mundi bonorum tractatos ei vix, magna incorrupte qui ex. Mel lobortis indoctum ne, fugit scaevola per et, at prima dicit facilisis usu. Iuvaret minimum atomorum nec ne. Te populo salutatus usu, id quaeque detraxit necessitatibus mel. At nam solum inani ocurreret, est dicta aperiam tractatos et. Aliquam scribentur ex mel, usu partem fierent scripserit ut.
+
+						Sit ne causae copiosae voluptatum, at platonem accusamus reprehendunt nam, meliore probatus explicari vis ne. Congue atomorum expetendis te sed, usu id postea nusquam, his scaevola praesent interesset id. Eu mea meliore abhorreant mnesarchum, an mea rebum option intellegat. At per fastidii moderatius definitionem. In est tritani debitis platonem, est eu possim facilisis definitionem. Nibh liber malorum ei per, aperiam facilisis vim an. Officiis qualisque ne duo, ius ea magna commune consulatu.
+					</section>
+				</section>
+
+				<section className="text-box-container">
+					<section className="text-box" >
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod fermentum leo id mollis. Proin dignissim libero nibh, mattis dapibus lacus eleifend ac. Aliquam ac iaculis velit, et pulvinar sem. Cras erat mi, efficitur quis vulputate at, accumsan non neque. Ut risus ante, scelerisque eu leo vitae, tempus tristique eros. Donec arcu dolor, porttitor nec sem a, aliquet tempus lectus. Mauris odio lacus, tempus eu venenatis vitae, ornare vitae ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque eu magna libero. Duis mi lectus, accumsan non ante sed, porttitor suscipit augue. Donec quis elit mattis turpis dapibus fermentum a et sapien. Praesent dapibus ligula lacinia, vehicula diam non, malesuada ex. Integer eu lorem vel ligula mattis facilisis. Quisque sollicitudin tellus et urna rutrum vestibulum.
+					</section>
+				</section>
+				
 			</section>
-			
 
 
 			
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
-				
-				<defs>
-					<filter id="round">
-						<feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur" />    
-						<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
-						<feComposite in="SourceGraphic" in2="goo" operator="atop"/>
-					</filter>
-				</defs>
-			</svg>
 
 		</section>
 	);
