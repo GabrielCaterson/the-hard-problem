1c4628715169455996c5ffcea6b7d1fdb3dd20ef
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-02-25 22:43:42 -0500

    CSS edits.

--- src/App.js
+++ src/App.js
@@ -27,10 +27,10 @@ function App() {
 				<section className="text-box-container text-box-container-mug">
 					<section className="text-box-container-inner
 										text-box-container-inner-mug 
-										purple-background">
+										green-background">
 						<section className="text-box-container-inner-2">
 							<section className="text-box text-box-mug" >
-								The hard problem of consciousness is one of the hardest problems in philosophy today. In simple terms, it asks "why is it like something to have the experiences we have?" Why is it like something to see the <span className="red-text"> color red</span>, <span className="brown-text">smell coffee</span>, or taste <span className="blue-text">blueberries</span>? 
+								The hard problem of consciousness is one of the hardest problems in philosophy today. In simple terms, it asks "why is it like something to have the experiences we have?" Why is it like something to see the color red, smell coffee, or taste blueberries? 
 							</section>
 						</section>
 
@@ -50,7 +50,7 @@ function App() {
 					</section>
 
 					
-					<section className="mug-container red-background"> 
+					<section className="mug-container blue-background"> 
 						<section className="mug-container-inner"> 
 							<section className="mug"> 
 
@@ -65,7 +65,7 @@ function App() {
 
 				<section className="text-box-container text-box-container-bat">
 
-					<section className="bat-container blue-background"> 
+					<section className="bat-container yellow-background"> 
 						<section className="bat-container-inner"> 
 							<section className="bat"> 
 
@@ -76,7 +76,7 @@ function App() {
 
 					<section className="text-box-container-inner
 										text-box-container-inner-bat
-										yellow-background">
+										purple-background">
 						<section className="text-box text-box-bat" >
 							What is it like to be a bat? <br />
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -109,7 +109,7 @@ function App() {
 
 				<section className="text-box-container text-box-container-zombie">
 
-					<section className="zombie-container"> 
+					<section className="zombie-container blue-background"> 
 						<section className="zombie-container-inner"> 
 							<section className="mug zombie"> 
 
