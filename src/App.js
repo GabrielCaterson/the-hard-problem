28c5b1be05cdb5d1c2b8717d8aa87393de77d69f
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-02-28 22:37:53 -0500

    Added text.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 import gsap from "gsap";
+import SplitTextJS from 'split-text-js';
+
+let text
+const titles = gsap.utils.toArray('p');
+const tl = gsap.timeline();
+
+
+titles.forEach(title => {
+	const splitTitle = new SplitTextJS(title);
+	
+	tl
+		.from(splitTitle.chars, {
+			opacity: 0,
+			y: 80,
+			rotateX: -90,
+			stagger: .005
+		}, "<")
+
+		.to(splitTitle.chars, {
+			opacity: 1,
+			y: 0,
+			rotateX: 0,
+			stagger: .005
+		}, "<1")
+
+});
+
+
 
 
 function App() {
@@ -32,21 +60,21 @@ function App() {
 										text-box-container-inner-mug 
 										green-background">
 						<section className="text-box-container-inner-2">
-							<section className="text-box text-box-mug" >
+							<p className="text-box text-box-mug" >
 								The hard problem of consciousness is one of the hardest problems in philosophy today. In simple terms, it asks "why is it <span className="like-something blue-text">like something</span> to have the experiences we have?" Why is it like something to see the color red, smell coffee, or taste blueberries? 
-							</section>
+							</p>
 						</section>
 
 						<section className="text-box-container-inner-2">
-							<section className="text-box text-box-mug" >
+							<p className="text-box text-box-mug" >
 								The trouble is, everything in the universe is physical, but consciousness seems to be something different. The experience of red seems to be non-physical, or as the philosophers call it, phenomenal. 
-							</section>
+							</p>
 						</section>
 
 						<section className="text-box-container-inner-2">
-							<section className="text-box text-box-mug" >
+							<p className="text-box text-box-mug" >
 								Many solutions to the hard problem have been proposed. Some accept the hard problem, and others say there is no consciousness in this "what it's like" sense. Let's explore some of the arguments.
-							</section>
+							</p>
 						</section>
 
 
@@ -80,10 +108,16 @@ function App() {
 					<section className="text-box-container-inner
 										text-box-container-inner-bat
 										purple-background">
-						<section className="text-box text-box-bat" >
+						<p className="text-box text-box-bat" >
 							What is it like to be a bat? <br />
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</section>
+							We know roughly what it's like to be other people because they are very similar to us, but what would it be like to be a bat? 
+								
+								<br />
+
+							- Bats are different from us 
+							
+							--Scientific theories are objective but our experiences are subjective
+						</p>
 					</section>
 				</section>
 
@@ -93,9 +127,9 @@ function App() {
 									text-box-container-mary
 									black-background">
 					<section className="text-box-container-inner text-box-container-inner-mary">
-						<section className="text-box text-box-1" >
+						<p className="text-box text-box-1" >
 							Mary's room.
-						</section>
+						</p>
 					</section>
 
 					
@@ -124,9 +158,9 @@ function App() {
 					<section className="text-box-container-inner
 										text-box-container-inner-zombie
 										green-background">
-						<section className="text-box text-box-1" >
+						<p className="text-box text-box-1" >
 							Philosophical zombies.
-						</section>
+						</p>
 
 					</section>
 				</section>
