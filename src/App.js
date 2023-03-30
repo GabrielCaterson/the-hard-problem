80156739ee75da796af8988ab4a5cb4e0a076e46
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-03-30 00:18:00 -0400

    CSS edits.

--- src/App.js
+++ src/App.js
@@ -173,14 +173,22 @@ function App() {
 					<section className="text-box-container-inner
 										text-box-container-inner-bat
 										purple-background">
-						<p className="text-box text-box-bat" >
+						{/*<p className="text-box text-box-bat" >
 							What is it like to be a bat? <br /><br />
 							We know roughly what it's like to be other humans because they are very similar to us, but what would it be like to be a bat? We may know that they experience life differently from us, using echolocation, and we can maybe consider analogous experiences, but we can never know what it is like for a bat to be a bat, having lived an entire life solely as a bat. 
 
 
 							<br /><br />
 							What this shows us is that scientific theories are objective, but our experiences are subjective.
-						</p>
+						</p>*/}
+
+						<AnimatedText text={ 
+							<p> What is it like to be a bat? <br /><br />
+							We know roughly what it's like to be other humans because they are very similar to us, but what would it be like to be a bat? We may know that they experience life differently from us, using echolocation, and we can maybe consider analogous experiences, but we can never know what it is like for a bat to be a bat, having lived an entire life solely as a bat. 
+
+
+							<br /><br />
+							What this shows us is that scientific theories are objective, but our experiences are subjective. </p> }/>
 
 						<section className="reading-section">
 							<ReadMore color="yellow-background black-text" link="https://warwick.ac.uk/fac/cross_fac/iatl/study/ugmodules/humananimalstudies/lectures/32/nagel_bat.pdf" text="Read paper"/>
@@ -195,11 +203,18 @@ function App() {
 									text-box-container-mary
 									">
 					<section className="text-box-container-inner text-box-container-inner-mary black-background">
-						<p className="text-box text-box-1" >
+						{/*<p className="text-box text-box-1" >
 							Mary's room. <br /><br />
 
 							Imagine a color scientist named Mary. She has lived her entire life in a black and white room, and can only investigate the world using a black and white television monitor. That being said, she knows everything there is to know about the physical descriptions of color, and how the human body interacts with it. Will Mary learn anything when she sees color for the first time?
-						</p>
+						</p>*/}
+
+						<AnimatedText text={ 
+							<p> Mary's room. <br /><br />
+
+							Imagine a color scientist named Mary. She has lived her entire life in a black and white room, and can only investigate the world using a black and white television monitor. That being said, she knows everything there is to know about the physical descriptions of color, and how the human body interacts with it. Will Mary learn anything when she sees color for the first time? </p> }/>
+
+						
 
 						<section className="reading-section">
 							<ReadMore color="white-background black-text" link="" text="Read paper"/>
@@ -237,12 +252,18 @@ function App() {
 					<section className="text-box-container-inner
 										text-box-container-inner-zombie
 										green-background">
-						<p className="text-box text-box-1" >
+						{/*<p className="text-box text-box-1" >
 							Philosophical zombies. <br /><br />
 							Imagine a person physically identical to a normal person, but who doesn't have a conscious experience. We'll call this person a philosophical zombie. There is nothing it is like to be a zombie. Still, they will behave the same way a normal person would. For instance, if someone poked a zombie with a sharp object, they would cry out as if they were in pain, but they wouldn't feel anything. Some zombies might even read websites about consciousness! <br /><br />
 
 							If it's at least possible for there to be a world where this kind of person exists, then it must be the case that consciousness exists, and isn't physical.
-						</p>
+						</p>*/}
+
+						<AnimatedText text={ 
+							<p> Philosophical zombies. <br /><br />
+							Imagine a person physically identical to a normal person, but who doesn't have a conscious experience. We'll call this person a philosophical zombie. There is nothing it is like to be a zombie. Still, they will behave the same way a normal person would. For instance, if someone poked a zombie with a sharp object, they would cry out as if they were in pain, but they wouldn't feel anything. Some zombies might even read websites about consciousness! <br /><br />
+
+							If it's at least possible for there to be a world where this kind of person exists, then it must be the case that consciousness exists, and isn't physical. </p> }/>
 
 						<section className="reading-section">
 							<ReadMore color="blue-background white-text" link="https://warwick.ac.uk/fac/cross_fac/iatl/study/ugmodules/humananimalstudies/lectures/32/nagel_bat.pdf" text="Read paper"/>
