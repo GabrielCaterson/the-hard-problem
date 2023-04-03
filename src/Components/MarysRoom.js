daf48a21803655f5a7934c80c6c048cfde3b7e9b
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-04-02 23:23:19 -0400

    Mary's room three.js work.

--- src/Components/MarysRoom.js
+++ src/Components/MarysRoom.js
@@ -1,17 +1,13 @@
 import React, { Component, useRef, useState } from 'react';
-import * as THREE from "three";
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'; 
 import { OrbitControls, useTexture } from '@react-three/drei';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import Panorama from '../marys_room.jpeg';
 
 
-
-
 function Room(props) {
-    
     const texture = useLoader(TextureLoader, Panorama );
-    //const colorMap = useTexture('/pictures/pana.jpg');
+    //texture.flipY = false;
 
 	// This reference gives us direct access to the THREE.Mesh object
 	const ref = useRef()
@@ -31,19 +27,18 @@ function Room(props) {
 		onClick={(event) => click(!clicked)}
 		onPointerOver={(event) => hover(false)}
 		onPointerOut={(event) => hover(false)}>
-		{/*<boxGeometry args={[1, 1, 1]} />*/}
-        <sphereGeometry args={[-500, 60, 40]} />
-        scale={[1, 1, 1]}
+        {/*scale={[1, 10, 1]}*/}
         position={[0, 0, 0.1]}
-        noZoom={[true]}
+        {/*noZoom={[true]}*/}
+        rotation={[-1,-1,-1]}
+        <sphereGeometry args={[3, 60, 40]} />
+
 
         <perspectiveCamera args={[75, window.innerWidth / window.innerHeight, 0.1, 1000]} />
-		{/*<meshStandardMaterial color={hovered ? 'hotpink' : 'gray'} />*/}
-        <meshBasicMaterial map={texture} />
+        <meshBasicMaterial map={texture}/>
 	  </mesh>
 	)
 }
-
 
 
 export class MarysRoom extends Component {
@@ -53,18 +48,16 @@ export class MarysRoom extends Component {
 		this.state = {
         }
 	}
-    
+
 	render() {
 
         return (
 			<section className="marys-room"> 
                 <Canvas>
-                    <ambientLight intensity={0.5} />
-                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                    <pointLight position={[-10, -10, -10]} />
-                    {/*<Box position={[-1.2, 0, 0]} />
-                    <Box position={[1.2, 0, 0]} />
-                    <Box position={[0, 2.4, 0]} />*/}
+                    <ambientLight intensity={0} />
+                    {/*<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                    <pointLight position={[-10, -10, -10]} />*/}
+
                     <Room position={[0, 0, 0]} />
 
                     <OrbitControls />
