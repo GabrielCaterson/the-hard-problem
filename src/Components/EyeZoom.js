d4ae97feef146d90554aa88294a6b9eee407e16d
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-04-09 23:34:23 -0400

    Scroll CSS edits.

--- src/Components/EyeZoom.js
+++ src/Components/EyeZoom.js
@@ -1,23 +1,26 @@
+window.onload = function() {
+
 // JavaScript code
-// Get the eye and eye-container elements
-let eye = document.querySelector(".eye");
-let eyeContainer = document.querySelector(".eye-container");
+    // Get the eye and eye-container elements
+    let eye = document.querySelector(".eye");
+    let eyeContainer = document.querySelector(".eye-container");
 
-// Get the height of the eye-container element
-let eyeContainerHeight = eyeContainer.offsetHeight;
+    // Get the height of the eye-container element
+    let eyeContainerHeight = eyeContainer.offsetHeight;
 
-// Add a scroll event listener to the eye-container element
-eyeContainer.addEventListener("scroll", function() {
-    // Get the current scroll position of the eye-container element
-    let scrollTop = eyeContainer.scrollTop;
+    // Add a scroll event listener to the eye-container element
+    eyeContainer.addEventListener("scroll", function() {
+        // Get the current scroll position of the eye-container element
+        let scrollTop = eyeContainer.scrollTop;
 
-    // Calculate the percentage of the scroll position relative to the height of the eye-container element
-    let percentage = scrollTop / eyeContainerHeight;
+        // Calculate the percentage of the scroll position relative to the height of the eye-container element
+        let percentage = scrollTop / eyeContainerHeight;
 
-    // Set the animation-play-state property of the eye element to running
-    eye.style.animationPlayState = "running";
+        // Set the animation-play-state property of the eye element to running
+        eye.style.animationPlayState = "running";
 
-    // Set the animation-delay property of the eye element to a negative value based on the percentage
-    // This will make the animation start from a certain point depending on the scroll position
-    eye.style.animationDelay = -percentage + "s";
-});
+        // Set the animation-delay property of the eye element to a negative value based on the percentage
+        // This will make the animation start from a certain point depending on the scroll position
+        eye.style.animationDelay = -percentage + "s";
+    });
+}
