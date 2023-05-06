e178d1c3364c1a6f060588bf3a19e7374aa63d21
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-05-05 23:53:23 -0400

    Gradient text animation test.

--- src/Components/AnimatedText.js
+++ src/Components/AnimatedText.js
@@ -42,7 +42,7 @@ const AnimatedText = (props) => {
     return (
         <section
             ref={ref}
-            className={`text-box-container-inner-2 text-animate-in`} >
+            className={`text-box-container-inner-2 text-animate-in ${inView ? 'text-animate-in-onscreen' : 'text-animate-in-offscreen'}`} >
 
             <section className={`text-box text-box-overview ${inView ? 'text-animate-in-onscreen' : 'text-animate-in-offscreen'}`} >
                 { props.text }
