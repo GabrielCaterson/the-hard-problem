74f6e5ee08f87df94167a2215fb8b4b07451996c
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-07-09 21:50:51 -0400

    CSS edits.

--- src/App.js
+++ src/App.js
@@ -65,12 +65,13 @@ function App() {
 									What is it like to be a bat? 
 								</span>
 								<br /><br />
-								We know roughly what it's like to be other humans because they are very similar to us, but what would it be like to be a bat? We may know that they experience life differently from us, using echolocation, and we can maybe consider analogous experiences, but we can never know what it is like for a bat to be a bat, having lived an entire life solely as a bat. 
+								We know roughly what it's like to be other humans because they are very similar to us, but what would it be like to be a bat? We may know that they experience life differently from us, using echolocation, and we can maybe consider analogous experiences, but we can never know what it is like for a bat to be a bat, having lived an entire life solely as a bat.
 
 
 								<br /><br />
 								What this shows us is that scientific theories are objective, but our experiences are subjective. 
-							</p> }/>
+							</p> 
+						}/>
 
 						<section className="reading-section">
 							<ReadMore color="black-background yellow-text" link="https://warwick.ac.uk/fac/cross_fac/iatl/study/ugmodules/humananimalstudies/lectures/32/nagel_bat.pdf" text="Read paper"/>
@@ -94,7 +95,8 @@ function App() {
 								<br /><br />
 
 								Imagine a color scientist named Mary. She has lived her entire life in a black and white room, and can only investigate the world using a black and white television monitor. That being said, she knows everything there is to know about the physical descriptions of color, and how the human body interacts with it. Will Mary learn anything when she sees color for the first time? 
-							</p> }/>
+							</p> 
+						}/>
 
 						
 
@@ -151,7 +153,8 @@ function App() {
 								Imagine a person physically identical to a normal person, but who doesn't have a conscious experience. We'll call this person a philosophical zombie. There is nothing it is like to be a philosophical zombie. Still, they will behave the same way a normal person would. For instance, if someone poked a zombie with a sharp object, they would cry out as if they were in pain, but they wouldn't feel anything. Some zombies might even read websites about consciousness! <br /><br />
 
 								If it's at least possible for there to be a world where this kind of person exists, then it must be the case that consciousness exists, and isn't physical. 
-								</p> }/>
+							</p> 
+						}/>
 
 						<section className="reading-section">
 							<ReadMore color="white-background blue-text" link="https://plato.stanford.edu/entries/zombies/" text="Read more"/>
@@ -175,7 +178,8 @@ function App() {
 								</span>
 								<br /><br />
 								Materialism is the view that everything is physical. There is no non-physical consciousness.
-							</p> }/>
+							</p> 
+						}/>
 
 						<section className="reading-section">
 							<ReadMore color="black-background white-text" link="https://plato.stanford.edu/entries/physicalism/" text="Read more"/>
@@ -214,7 +218,8 @@ function App() {
 								</span>
 								<br /><br />
 								Dualism is the view that the mind and the body are radically different kinds of things. 
-							</p> }/>
+							</p> 
+						}/>
 
 						<section className="reading-section">
 							<ReadMore color="white-background purple-text" link="https://plato.stanford.edu/entries/dualism/" text="Read more"/>
@@ -237,7 +242,8 @@ function App() {
 								</span>
 								<br /><br />
 								Panpsychism is the view that consciousness is fundamental and ubiquitous. In simpler terms, consciousness is everywhere. That isn't to say that everything including rocks are conscious, but more that everything including rocks are made of conscious parts.
-							</p> }/>
+							</p> 
+						}/>
 
 						<section className="reading-section">
 							<ReadMore color="white-background black-text" link="https://plato.stanford.edu/entries/panpsychism/" text="Read more"/>
@@ -273,7 +279,8 @@ function App() {
 								</span>
 								<br /><br />
 								Idealism is the view that everything is consciousness. Consciousness is the ultimate foundation of all reality. There is no non-conscious matter. Consciousness is all there is.
-							</p> }/>
+							</p> 
+						}/>
 
 						<section className="reading-section">
 							<ReadMore color="black-background green-text" link="https://plato.stanford.edu/Archives/Win2017/entries/idealism/" text="Read more"/>
